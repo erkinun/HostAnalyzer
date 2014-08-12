fae5a9e6febc4b56e8ad6156b25f441389beb765
lost_mohican <erkinun@gmail.com>
2014-08-12 22:05:19 +0200

    wrong version of analyze done

--- src/main/java/Analyzer.java
+++ src/main/java/Analyzer.java
@@ -68,8 +68,14 @@ public class Analyzer {
 
         //empty hosts of each type
         emptyHosts = new HashMap<>();
+        emptyHosts.put(InstanceType.M1, 0);
+        emptyHosts.put(InstanceType.M2, 0);
+        emptyHosts.put(InstanceType.M3, 0);
         //full hosts of each type
         fullHosts = new HashMap<>();
+        fullHosts.put(InstanceType.M1, 0);
+        fullHosts.put(InstanceType.M2, 0);
+        fullHosts.put(InstanceType.M3, 0);
 
         determineEmptyAndFull();
 
@@ -79,29 +85,14 @@ public class Analyzer {
         Arrays.sort(hostArray, Host.BY_MOST_FILLED);
 
 
-        Map<InstanceType, Integer> mostFilled = new HashMap<>();
-        Map<InstanceType, Integer> mostOccupiedCount = new HashMap<>();
-        Map<InstanceType, Boolean> sweepDone = new HashMap<>();
-        for (Host host : hostArray) {
-            if (!host.isFull() && !sweepDone.get(host.getType())) {
-                int occupiedCount = mostOccupiedCount.get(host.getType());
+        String mostFilledLine = analyzeMostFilled(hostArray);
 
-                if (host.getOccupiedSlots() > occupiedCount) {
-                    mostOccupiedCount.put(host.getType(), occupiedCount);
-                    int mostFilledCount = mostFilled.get(host.getType());
-                    mostFilledCount++;
-                    mostFilled.put(host.getType(), mostFilledCount);
-                }
-                else {
-                    sweepDone.put(host.getType(), true);
-                }
-            }
-        }
+        String emptyLine = produceLine(emptyHosts);
+        String fullLine = produceLine(fullHosts);
 
-        //how many are those hosts
-        throw new RuntimeException();
+        return emptyLine + "\n" + fullLine + "\n" + mostFilledLine;
+
     }
-
 
     public void writeResults(String results) {
         String outFileName = "Statistics.txt";
@@ -124,5 +115,51 @@ public class Analyzer {
                 fullHosts.put(host.getType(), fullCount);
             }
         }
+    }
+
+    private String analyzeMostFilled(Host[] hostArray) {
+        Map<InstanceType, Integer> mostFilled = new HashMap<>();
+        mostFilled.put(InstanceType.M1, 0);
+        mostFilled.put(InstanceType.M2, 0);
+        mostFilled.put(InstanceType.M3, 0);
+
+        Map<InstanceType, Integer> mostOccupiedCount = new HashMap<>();
+        mostOccupiedCount.put(InstanceType.M1, 0);
+        mostOccupiedCount.put(InstanceType.M2, 0);
+        mostOccupiedCount.put(InstanceType.M3, 0);
+
+        Map<InstanceType, Boolean> sweepDone = new HashMap<>();
+        sweepDone.put(InstanceType.M1, false);
+        sweepDone.put(InstanceType.M2, false);
+        sweepDone.put(InstanceType.M3, false);
+
+        for (Host host : hostArray) {
+            if (!host.isFull() && !sweepDone.get(host.getType())) {
+                int occupiedCount = mostOccupiedCount.get(host.getType());
+
+                if (host.getOccupiedSlots() > occupiedCount) {
+                    mostOccupiedCount.put(host.getType(), occupiedCount);
+                    int mostFilledCount = mostFilled.get(host.getType());
+                    mostFilledCount++;
+                    mostFilled.put(host.getType(), mostFilledCount);
+                }
+                else {
+                    sweepDone.put(host.getType(), true);
+                }
+            }
+        }
+
+        return "MOST FILLED: M1=" + mostFilled.get(InstanceType.M1) + ","
+                + mostOccupiedCount.get(InstanceType.M1)
+                + "; M2=" + mostFilled.get(InstanceType.M2) + ","
+                + mostOccupiedCount.get(InstanceType.M2)
+                + "; M3=" + mostFilled.get(InstanceType.M3) + ","
+                + mostOccupiedCount.get(InstanceType.M3);
+    }
+
+    private String produceLine(Map<InstanceType, Integer> hosts) {
+        return "EMPTY: M1=" + hosts.get(InstanceType.M1) + "; M2="
+                + hosts.get(InstanceType.M2) + "; M3="
+                + hosts.get(InstanceType.M3) + ";";
     }
 }
